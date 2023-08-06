f555116bb091685e15351fc6d51ffc4fd111e539
Eugene Low <eugene.ws.low@gmail.com>
2023-08-06 09:40:41 +0800

    Buttons are loaded through json

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   let imagesData = [];
+  let buttonsData = [];
   let currentImageIndex = 0;
 
   const sliderImage = document.querySelector(".slider-image img");
   const sliderCaption = document.querySelector(".slider-caption p");
   const sliderLeftButton = document.querySelector(".slider-left");
   const sliderRightButton = document.querySelector(".slider-right");
+  const buttonsContainer = document.getElementById("buttons-container");
 
   function loadImagesData(callback) {
     fetch("images.json")
@@ -17,17 +19,35 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch((error) => console.error("Error loading images data:", error));
   }
 
+  function loadButtonsData(callback) {
+    fetch("buttons.json")
+      .then((response) => response.json())
+      .then((data) => {
+        buttonsData = data;
+        callback();
+      })
+      .catch((error) => console.error("Error loading buttons data:", error));
+  }
+
   function loadImage(index) {
     const imgData = imagesData[index];
     if (imgData) {
-      const imgElement = document.querySelector(".slider-image img");
-      
-      imgElement.src = imgData.image;
-      imgElement.alt = "Image " + (index + 1);
+      sliderImage.src = imgData.image;
+      sliderImage.alt = "Image " + (index + 1);
       sliderCaption.textContent = imgData.caption;
     }
   }
-  
+
+  function addButtons() {
+    buttonsData.forEach((buttonData) => {
+      const button = document.createElement("a");
+      button.href = buttonData.url;
+      button.className = "button";
+      button.textContent = buttonData.title;
+      buttonsContainer.appendChild(button);
+    });
+  }
+
   function navigate(direction) {
     currentImageIndex = (currentImageIndex + direction + imagesData.length) % imagesData.length;
     loadImage(currentImageIndex);
@@ -35,6 +55,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   loadImagesData(function () {
     loadImage(currentImageIndex);
+  });
+
+  loadButtonsData(function () {
+    addButtons();
 
     sliderLeftButton.addEventListener("click", function () {
       navigate(-1);
